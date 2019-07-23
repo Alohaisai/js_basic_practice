60d11a5624e1349a12b032e2e215ac293840a89b
alohaisai <mm70aloha@gmail.com>
2019-07-23 15:08:38 +0900

    ffirst commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let score ='100';
+
+if (score >= 80) {
+  console.log(`${score}は80点以上なので合格です`)
+}if (int <=79 && int >=50) {
+  console.log(`${score}は50点以上なので惜しいです`);
+}if (int <=49 && int >=30) {
+  console.log(`${score}は30点以上なので残念です`);
+}
