61e815bdfc38bfcaf94fadc0e49e384854f62adb
alohaisai <mm70aloha@gmail.com>
2019-07-23 15:30:20 +0900

    3の倍数と5の倍数の時にメッセージを追加
    for文とif文を追加しました

--- app.js
+++ app.js
@@ -8,10 +8,24 @@ if (score >= 80) {
   console.log(`${score}は30点以上なので残念です`);
 }
 
+
+// ifとforの処理
 for (let i = 1; i<=30; i++){
   if (i % 3 ==0) {
     console.log ('crazy');
   }else{
+    console.log(i);
+  }
+}
+
+// 5の倍数と3の倍数の時にメッセージ
+// それ以外は数字
+// 1～100
+
+for (let i = 1; i<=100; i++) {
+  if( i % 3 == 0 || i % 5 == 0) {
+    console.log('crazy');
+  } else {
     console.log(i);
   }
 }
